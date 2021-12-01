66ec496f17027920962383e16bf35ddb17cfa115
Plane-walker <a990990@163.com>
2021-12-01 18:50:08 +0800

    Init gnn.

---
+++ dock/gnn/node_classification.py
@@ -0,0 +1,12 @@
+class NodeClassification:
+    def __init__(self):
+        pass
+
+    def switch_community(self):
+        pass
+
+    def community_info(self):
+        pass
+
+    def community_config(self):
+        pass
