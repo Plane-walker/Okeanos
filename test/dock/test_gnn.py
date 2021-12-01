6377ebcff0631be5850c49132b59996af72ab973
Plane-walker <a990990@163.com>
2021-12-01 19:06:38 +0800

    Add unitest.

---
+++ test/dock/test_gnn.py
@@ -0,0 +1,16 @@
+import unittest
+from dock.gnn.node_classification import NodeClassification
+
+
+class TestNodeClassification(unittest.TestCase):
+    def test_switch_community(self):
+        node_classification = NodeClassification()
+        pass
+
+    def test_community_info(self):
+        node_classification = NodeClassification()
+        pass
+
+    def test_community_config(self):
+        node_classification = NodeClassification()
+        pass
