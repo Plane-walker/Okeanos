44379fffb313a3a6381083ea4ed550ac091c8774
Plane-walker <a990990@163.com>
2021-12-23 12:01:46 +0800

    Fix router test bug. #69

--- test/dock/router/test_router.py
+++ test/dock/router/test_router.py
@@ -12,19 +12,19 @@ from interface.dci.dci_pb2 import (
 
 class TestRouter(unittest.TestCase):
     
-    # def test_next_node(self):
-    #     router = Router()
-    #     ids = []
-    #     TEST_NUM = 5
-    #     for _ in range(TEST_NUM):
-    #         identifier = uuid.uuid4().fields[0]
-    #         router.paths[identifier] = Chain(identifier=identifier)
-    #         ids.append(identifier)
-    #     for id in ids:
-    #         self.assertTrue(id in router.paths)
-    #         self.assertEqual(id, router.next_node(Chain(identifier=id)).identifier)
-    #     for _ in range(TEST_NUM):
-    #         self.assertIsNone(router.next_node(Chain(identifier=uuid.uuid4().fields[0])))
+    def test_next_node(self):
+        router = Router()
+        ids = []
+        TEST_NUM = 5
+        for _ in range(TEST_NUM):
+            identifier = uuid.uuid4().fields[0]
+            router.paths[identifier] = Chain(identifier=identifier)
+            ids.append(identifier)
+        for id in ids:
+            self.assertTrue(id in router.paths)
+            self.assertEqual(id, router.next_node(Chain(identifier=id)).identifier)
+        for _ in range(TEST_NUM):
+            self.assertIsNone(router.next_node(Chain(identifier=uuid.uuid4().fields[0])))
 
     def test_client(self):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
