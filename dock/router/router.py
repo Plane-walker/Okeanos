a58cc657a51389262ce16e6b7ce177ebacb54f8e
Bao <baoayong@qq.com>
2021-12-23 17:09:41 +0800

    Add data_chain and add config for Router. Ref #71

--- dock/router/router.py
+++ dock/router/router.py
@@ -5,6 +5,8 @@ __all__ = [
 
 import random
 import grpc
+import os
+import yaml
 from interface.dci.dci_pb2 import Chain as DciChain
 from interface.dci.dci_pb2 import (
     RequestRouterInfo,
@@ -20,12 +22,6 @@ from interface.bci.bci_pb2 import (
     RequestGossipQueryPath,
     RequestGossipCallBack,
 )
-
-
-MIN_LANES = 3
-MIN_SEACHER = 3
-SUCCESS_CODE = 0
-DEFAULT_TTL = 10
 
 
 class Chain:
@@ -48,46 +44,68 @@ class Chain:
 
 class Router:
 
-    def __init__(self) -> None:
+    def __init__(self, config_path=None) -> None:
         self.node_id = 0
-        self.lanes = set()
+        self.data_chain_id = 0
         
-        # Route table is a dict whose key is target id in uint type and whose value
-        # is Lane Chain in Chain type.
+        # Key is the id of Router Chain
+        # Value contains ip:port and some other info
+        self.lanes = dict()
+        
+        # Route table is a dict whose key is target id in uint type and 
+        # whose value is Lane Chain in Chain type.
         self.route = dict()
-        self.rid = 0
         
-        return None
+        if config_path is None:
+            config_dir = os.path.dirname(__file__)
+            config_path = os.path.join(config_dir, 'default_router_config.yaml')
+        with open(config_path) as f:
+            config = yaml.load(f, Loader=yaml.Loader)
+            self.configure(config['router'])
+    
+    def configure(self, config: dict, config_key=None):
+        if not isinstance(config, dict):
+            return
+        if config_key is None:
+            keys = ('ttl', 'min_router_chain', 'min_search')
+            for key in keys:
+                if key not in config:
+                    raise KeyError
+                self.__setattr__(key, config[key])
+        else:
+            if config_key not in config:
+                raise KeyError
+            self.__setattr__(config_key, config[config_key])
 
     def next_node(self, target: Chain) -> Chain:
-        ttl = DEFAULT_TTL
+        ttl = self.ttl
         paths = []
         id = target.identifier
         while True:
             if id in self.route:
                 break
-            code = self.gossip(Chain(identifier=self.rid), target, ttl, paths)
+            code = self.gossip(Chain(identifier=self.data_chain_id), target, ttl, paths)
             if code is None:
                 return None
-            ttl += DEFAULT_TTL
+            ttl += self.ttl
             # TODO Sleep some time
         return self.route[target.identifier]
 
     def gossip(self, source: Chain, target: Chain, ttl, paths: list):
         num_of_neighbours = len(self.lanes)
-        if num_of_neighbours <= MIN_LANES:
+        if num_of_neighbours <= self.min_router_chain:
             return None
         selecteds = set()
-        if num_of_neighbours < MIN_SEACHER:
+        if num_of_neighbours < self.min_seacher:
             for lane in self.lanes:
                 selecteds.add(lane)
         else:
             while True:
-                if len(selecteds) == MIN_SEACHER:
+                if len(selecteds) == self.min_seacher:
                     break
                 selecteds.add(self.lanes[random.randint(0, num_of_neighbours-1)])
         self.transmit_to_others(selecteds, source, target, ttl=ttl, paths=paths)
-        return SUCCESS_CODE
+        return grpc.StatusCode.OK
 
     def transmit_to_others(self, lanes, source: Chain, target: Chain, ttl, paths: list):
         for lane in lanes:
@@ -117,7 +135,7 @@ class Router:
                     res = stub.GossipCallBack(req)
 
     def info(self, req: RequestRouterInfo) -> ResponseRouterInfo:
-        res = ResponseRouterInfo(code=SUCCESS_CODE,
+        res = ResponseRouterInfo(code=grpc.StatusCode.OK,
                                  data=('Route Table:\n'+str(self.route)).encode('utf-8'),
                                  info="Return route table info!")
         return res
@@ -128,12 +146,14 @@ class Router:
             paths.append(Chain(path))
         self.route[req.source.identifier] = paths[-1]
         if req.ttl <= 0:
-            return ResponseRouterTransmit(code=SUCCESS_CODE)
-        if req.target.identifier in self.route:
+            return ResponseRouterTransmit(code=grpc.StatusCode.OK)
+        if req.target.identifier == self.data_chain_id:
+            self.callback_to_finder(req.target, req.source, paths)
+        elif req.target.identifier in self.route:
             self.callback_to_finder(req.target, req.source, paths)
         else:
             self.gossip(req.source, req.target, ttl=req.ttl-1, paths=paths)
-        return ResponseRouterTransmit(code=SUCCESS_CODE)
+        return ResponseRouterTransmit(code=grpc.StatusCode.OK)
 
     def callback(self, req: RequestRouterPathCallback) -> ResponseRouterPathCallback:
         paths = []
@@ -142,6 +162,6 @@ class Router:
         self.route[req.source.identifier] = paths[-1]
         for lane in self.lanes:
             if req.target.identifier == lane.identifier:
-                return RequestRouterPathCallback(code=SUCCESS_CODE)
+                return RequestRouterPathCallback(code=grpc.StatusCode.OK)
         self.callback_to_finder(req.source, req.target, paths[:-1])
-        return RequestRouterPathCallback(code=SUCCESS_CODE)
+        return RequestRouterPathCallback(code=grpc.StatusCode.OK)
