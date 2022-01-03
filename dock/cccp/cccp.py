066c1e56550e0b52f42d7bacadc21a8c40f849c6
ye_gitee <xueye_liu@163.com>
2022-01-03 15:22:37 +0800

    Get targetID and query router path with test. Ref #78 #28 #11

--- dock/cccp/cccp.py
+++ dock/cccp/cccp.py
@@ -8,7 +8,6 @@ class CrossChainCommunicationProtocol:
     def __init__(self, router):
         self.next_router_path = None
         self.targetID = None
-        self.data = {'target_id': '1234', 'node_id': '2345'}
         self.router = router
 
     def get_target_id(self, request_tx_passage):
@@ -18,4 +17,3 @@ class CrossChainCommunicationProtocol:
 
     def query_route_path(self):
         self.next_router_path = self.router.next_node(self.targetID)
-    
