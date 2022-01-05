c4b3cd4670bf1a16fc9861c0ad6c0a9838ae7de1
Plane-walker <a990990@163.com>
2022-01-05 15:09:12 +0800

    Init manager. #63

---
+++ dock/manager/manager.py
@@ -0,0 +1,17 @@
+__all__ = [
+    'ChainManager'
+]
+
+
+class ChainManager:
+    def __init__(self):
+        pass
+
+    def create_chain(self):
+        pass
+
+    def join_chain(self):
+        pass
+
+    def leave_chain(self):
+        pass
