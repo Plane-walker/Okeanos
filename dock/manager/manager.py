b481b28bf2d466adbbb371eccbde0d0165b6e9d2
Plane-walker <a990990@163.com>
2022-01-06 16:10:56 +0800

    Create island chain. #107

--- dock/manager/manager.py
+++ dock/manager/manager.py
@@ -2,13 +2,25 @@ __all__ = [
     'ChainManager'
 ]
 
+import os
+import subprocess
+import signal
+
 
 class ChainManager:
-    def __init__(self):
-        pass
+    def __init__(self, island_process):
+        self.island_process = island_process
 
-    def create_chain(self):
-        pass
+    def create_chain(self, chain_type):
+        if chain_type == 'island':
+            self.island_process = subprocess.Popen(f"tendermint start --home /root/island &> /dev/null",
+                                                   shell=True,
+                                                   stdout=subprocess.PIPE,
+                                                   preexec_fn=os.setsid)
+
+    def stop_chain(self, chain_type):
+        if chain_type == 'island':
+            os.killpg(os.getpgid(self.island_process.pid), signal.SIGTERM)
 
     def join_chain(self):
         pass
